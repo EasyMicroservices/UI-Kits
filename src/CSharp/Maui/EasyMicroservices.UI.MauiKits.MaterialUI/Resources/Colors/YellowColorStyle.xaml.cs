fb1e0862d97feb555f796f29e4b2b53f49f50348
Ali Yousefi <ali.visual.studio@gmail.com>
2023-12-02 14:58:04 +0330

    add YellowColorStyle

---
+++ src/CSharp/Maui/EasyMicroservices.UI.MauiKits.MaterialUI/Resources/Colors/YellowColorStyle.xaml.cs
@@ -0,0 +1,9 @@
+namespace EasyMicroservices.UI.MauiKits.MaterialUI.Resources.Colors;
+
+public partial class YellowColorStyle : ResourceDictionary
+{
+	public YellowColorStyle()
+	{
+		InitializeComponent();
+	}
+}
