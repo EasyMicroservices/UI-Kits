aed0b3e91c7b73c53d3c7c1291af6ed222fd4429
Ali Yousefi <ali.visual.studio@gmail.com>
2023-12-02 14:57:21 +0330

    add LimeColorStyle

---
+++ src/CSharp/Maui/EasyMicroservices.UI.MauiKits.MaterialUI/Resources/Colors/LimeColorStyle.xaml.cs
@@ -0,0 +1,9 @@
+namespace EasyMicroservices.UI.MauiKits.MaterialUI.Resources.Colors;
+
+public partial class LimeColorStyle : ResourceDictionary
+{
+	public LimeColorStyle()
+	{
+		InitializeComponent();
+	}
+}
