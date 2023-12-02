048a18ab4af3c892a7a5193b0a70bd86f5f9fe49
Ali Yousefi <ali.visual.studio@gmail.com>
2023-12-02 14:59:44 +0330

    DeepOrangeColorStyle

---
+++ src/CSharp/Maui/EasyMicroservices.UI.MauiKits.MaterialUI/Resources/Colors/DeepOrangeColorStyle.xaml.cs
@@ -0,0 +1,9 @@
+namespace EasyMicroservices.UI.MauiKits.MaterialUI.Resources.Colors;
+
+public partial class DeepOrangeColorStyle : ResourceDictionary
+{
+	public DeepOrangeColorStyle()
+	{
+		InitializeComponent();
+	}
+}
