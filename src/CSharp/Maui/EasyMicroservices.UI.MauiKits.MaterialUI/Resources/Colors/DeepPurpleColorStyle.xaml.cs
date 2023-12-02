61b227f75dc9e26ee301757668947a7256d07994
Ali Yousefi <ali.visual.studio@gmail.com>
2023-12-02 14:52:07 +0330

    add DeepPurpleColorStyle

---
+++ src/CSharp/Maui/EasyMicroservices.UI.MauiKits.MaterialUI/Resources/Colors/DeepPurpleColorStyle.xaml.cs
@@ -0,0 +1,9 @@
+namespace EasyMicroservices.UI.MauiKits.MaterialUI.Resources.Colors;
+
+public partial class DeepPurpleColorStyle : ResourceDictionary
+{
+	public DeepPurpleColorStyle()
+	{
+		InitializeComponent();
+	}
+}
